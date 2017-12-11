14eaaa7d7298cd919c0b2763d7f460daff0538f2
Ben Swift <ben@benswift.me>
2017-12-12 10:33:44 +1100

    change "extension activated" message

--- src/extension.ts
+++ src/extension.ts
@@ -21,7 +21,7 @@ export function activate(context: vscode.ExtensionContext) {
     
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "extempore" is now active!');
+    console.log('Extempore extension activated.');
     
     // send sexpr
     let sendSexprDisposable = vscode.commands.registerCommand('extension.xtmsend', () => {
