2172b9c19bda581ec8f3b508787edcf37c5ad3e2
Ben Swift <ben@benswift.me>
2017-12-19 14:54:42 +0800

    refactor: make an Extempore class

--- src/extension.ts
+++ src/extension.ts
@@ -8,60 +8,69 @@ import { setTimeout } from 'timers';
 
 import { xtmIndent, xtmInSexpr, xtmTopLevelSexpr, xtmSexprToString } from './sexpr';
 
-let socket: net.Socket;
-let extemporeTerminal: vscode.Terminal;
-
-function crlf2lf(strin: string): string {
-    //console.log("CRLF_IN:\n", strin);
-    let strout = strin.replace(/(\r\n|\n|\r)/gm, "\x0A");
-    //console.log("LF_OUT:\n", strout);
-    return strout;
-}
-
-function evalString(str: string) {
-    try {
-        socket.write(crlf2lf(str).concat("\r\n"));
-    } catch (error) {
-        vscode.window.showErrorMessage("Extempore: error sending code to process---do you need to connect?")
-    }
-}
-
-function extemporeOnPath(): boolean {
-    if (os.platform() === "win32") {
-        return false;
-    } else {
-        return spawnSync("which", ["extempore"]).status === 0;
-    }
-}
-
-function getShareDirPath() {
-    let config = vscode.workspace.getConfiguration("extempore");
-    if (config.has("sharedir")) {
-        return config.get("sharedir");
-    } else if (vscode.workspace.rootPath) {
-        return vscode.workspace.rootPath;
-    } else {
-        return undefined;
-    }
-}
-
-function blinkRange(range: vscode.Range) {
-    let decoration = vscode.window.createTextEditorDecorationType({
-        color: "#000",
-        backgroundColor: "#fd971f"
-    });
-    vscode.window.activeTextEditor.setDecorations(decoration, [range]);
-    setTimeout(() => decoration.dispose(), 500);
-}
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    let extempore = new Extempore();
 
-    console.log('Extempore extension activated.');
+    context.subscriptions.push(vscode.commands.registerCommand('extension.xtmstart', extempore.startCommand));
+    context.subscriptions.push(vscode.commands.registerCommand('extension.xtmconnect', extempore.connectCommand));
+    context.subscriptions.push(vscode.commands.registerCommand('extension.xtmeval', extempore.evalCommand));
+    context.subscriptions.push(vscode.commands.registerCommand('extension.xtmdisconnect', extempore.disconnectCommand));
+
+    context.subscriptions.push(extempore);
+}
+
+class Extempore {
+
+    private _socket: net.Socket;
+    private _terminal: vscode.Terminal;
+
+    constructor() {
+        // nothing to do here - start with "Extempore Start" command
+    }
+
+    // utility functions
+    crlf2lf(str: string): string {
+        return str.replace(/(\r\n|\n|\r)/gm, "\x0A");
+    }
+
+    eval(str: string) {
+        try {
+            this._socket.write(this.crlf2lf(str).concat("\r\n"));
+        } catch (error) {
+            vscode.window.showErrorMessage("Extempore: error sending code to process---do you need to connect?")
+        }
+    }
+
+    isExtemporeOnSystemPath(): boolean {
+        if (os.platform() === "win32") {
+            return false;
+        } else {
+            return spawnSync("which", ["extempore"]).status === 0;
+        }
+    }
+
+    getExtemporePath() {
+        let config = vscode.workspace.getConfiguration("extempore");
+        if (config.has("sharedir")) {
+            return config.get("sharedir");
+        } else if (vscode.workspace.rootPath) {
+            return vscode.workspace.rootPath;
+        } else {
+            return undefined;
+        }
+    }
+
+    evalBlink(range: vscode.Range) {
+        let decoration = vscode.window.createTextEditorDecorationType({
+            color: "#000",
+            backgroundColor: "#fd971f"
+        });
+        vscode.window.activeTextEditor.setDecorations(decoration, [range]);
+        setTimeout(() => decoration.dispose(), 500);
+    }
 
     // eval sexpr
-    let evalSexprDisposable = vscode.commands.registerCommand('extension.xtmeval', ()  => {
+    evalCommand() {
         let editor = vscode.window.activeTextEditor;
         let document = editor.document;
         let codeString = "";
@@ -70,104 +79,100 @@ export function activate(context: vscode.ExtensionContext) {
             // if no code is selected, select current top-level form
             codeString = document.getText(editor.selection);
             // "flash" the eval'ed code
-            blinkRange(editor.selection);
-        } else  {
+            this.evalBlink(editor.selection);
+        } else {
             let txtstr = document.getText();
             // make sure we are LF ends for Extempore comms
             let pos = vscode.window.activeTextEditor.selection.active;
             let sexpr = xtmTopLevelSexpr(txtstr, document.offsetAt(pos) - 1);
             codeString = xtmSexprToString(txtstr, sexpr);
             // "flash" the eval'ed code
-            blinkRange(new vscode.Range(document.positionAt(sexpr.start),
-                                        document.positionAt(sexpr.end+1)));
+            this.evalBlink(new vscode.Range(document.positionAt(sexpr.start),
+                document.positionAt(sexpr.end + 1)));
         }
-        evalString(codeString);
-    });
-    context.subscriptions.push(evalSexprDisposable);
+        this.eval(codeString);
+    };
 
     // start Extempore in a new Terminal
-    let startExtemporeDisposable = vscode.commands.registerCommand('extension.xtmstart', async () => {
+    startCommand(){
         // if there's already an Extempore terminal running, kill it
-        if (extemporeTerminal) {
-            extemporeTerminal.dispose();
+        if (this._terminal) {
+            this._terminal.dispose();
         }
         // find the path to the extempore folder
-        let sharedir = getShareDirPath();
+        let sharedir = this.getExtemporePath();
 
         if (!sharedir) {
             vscode.window.showErrorMessage("Extempore: can't find extempore folder.");
             return;
         }
 
-        extemporeTerminal = vscode.window.createTerminal("Extempore");
-        extemporeTerminal.show(true); // show, but don't steal focus
+        this._terminal = vscode.window.createTerminal("Extempore");
+        this._terminal.show(true); // show, but don't steal focus
 
         if (os.platform() === 'win32') {
-            extemporeTerminal.sendText(`cwd ${sharedir}`);
-            extemporeTerminal.sendText(`./extempore.exe`);
+            this._terminal.sendText(`cwd ${sharedir}`);
+            this._terminal.sendText(`./extempore.exe`);
         } else {
-            extemporeTerminal.sendText(`cd ${sharedir}`);
-            extemporeTerminal.sendText(extemporeOnPath() ? "extempore" : "./extempore");
+            this._terminal.sendText(`cd ${sharedir}`);
+            this._terminal.sendText(this.isExtemporeOnSystemPath() ? "extempore" : "./extempore");
         };
-    });
-    context.subscriptions.push(startExtemporeDisposable);
+    };
 
     // connect to extempore
-    let connectDisposable = vscode.commands.registerCommand('extension.xtmconnect', async () => {
+    async connectCommand(){
         let hostname: string = await vscode.window.showInputBox({ prompt: 'Hostname', value: 'localhost' });
         let portString: string = await vscode.window.showInputBox({ prompt: 'Port number', value: '7099' });
         let port: number = parseInt(portString);
 
         // create Extempore socket
-        socket = new net.Socket();
-        socket.setEncoding('ascii');
-        socket.setKeepAlive(true);
+        this._socket = new net.Socket();
+        this._socket.setEncoding('ascii');
+        this._socket.setKeepAlive(true);
 
         // set socket callbacks
-        socket.connect(port, hostname, () => {
+        this._socket.connect(port, hostname, () => {
             vscode.window.setStatusBarMessage(`Extempore: connected to port ${port}`);
         });
-        socket.on('data', (data) => {
+        this._socket.on('data', (data) => {
             vscode.window.setStatusBarMessage(data.toString());
         });
-        socket.on('close', () => {
+        this._socket.on('close', () => {
             vscode.window.setStatusBarMessage(`Extempore: connection to port ${port} closed`);
         });
-        socket.on('error', (err) => {
+        this._socket.on('error', (err) => {
             vscode.window.showErrorMessage(`Extempore: socket connection error "${err.message}"`);
         })
-    });
-    context.subscriptions.push(connectDisposable);
+    };
 
-    // connect to extempore
-    let disconnectDisposable = vscode.commands.registerCommand('extension.xtmdisconnect', () => {
-        socket.destroy();
-    });
-    context.subscriptions.push(disconnectDisposable);
+    disconnectCommand() {
+        this._socket.destroy();
+    }
+    
+    registerFormattingProvider(context: vscode.ExtensionContext) {
+        let indentDisposable = vscode.languages.registerOnTypeFormattingEditProvider('extempore', {
+            provideOnTypeFormattingEdits(document: vscode.TextDocument, position: vscode.Position, ch: string, options: vscode.FormattingOptions, token: vscode.CancellationToken): vscode.ProviderResult<vscode.TextEdit[]> {
+                let previousLines = new vscode.Position(0, 0);
+                let backRange = new vscode.Range(previousLines, position);
+                let txtstr = document.getText(backRange);
+                let indent = xtmIndent(txtstr);
 
-    // indentation
-    let indentDisposable = vscode.languages.registerOnTypeFormattingEditProvider('extempore', {
-        provideOnTypeFormattingEdits(document: vscode.TextDocument, position: vscode.Position, ch: string, options: vscode.FormattingOptions, token: vscode.CancellationToken): vscode.ProviderResult<vscode.TextEdit[]> {
-            let previousLines = new vscode.Position(0, 0);
-            let backRange = new vscode.Range(previousLines, position);
-            let txtstr = document.getText(backRange);
-            let indent = xtmIndent(txtstr);
+                vscode.window.activeTextEditor.edit((edit) => {
+                    let pos = vscode.window.activeTextEditor.selection.active;
+                    let startOfLine = new vscode.Position(pos.line, 0);
+                    let sol = new vscode.Range(startOfLine, pos);
+                    edit.delete(sol);
+                    let emptyStr = ' '.repeat(indent);
+                    edit.insert(startOfLine, emptyStr);
+                });
+                return null;
+            }
+        }, '\n');
+        context.subscriptions.push(indentDisposable);
+    }
 
-            vscode.window.activeTextEditor.edit((edit)=> {
-                let pos = vscode.window.activeTextEditor.selection.active;
-                let startOfLine = new vscode.Position(pos.line, 0);
-                let sol = new vscode.Range(startOfLine, pos);
-                edit.delete(sol);
-                let emptyStr = ' '.repeat(indent);
-                edit.insert(startOfLine,emptyStr);
-            });
-            return null;
-        }
-    }, '\n');
-    context.subscriptions.push(indentDisposable);
-}
-
-// this method is called when your extension is deactivated
-export function deactivate() {
-    socket.destroy();
+    dispose() {
+        this._socket.destroy();
+        this._terminal.dispose();
+    }
 }
